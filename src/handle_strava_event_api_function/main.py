e059e9e64af036afe39a9680dae871f05052fa10
GraysonCox <graysoncox98@gmail.com>
2024-11-07 20:01:51 -0600

    WIP

--- src/handle_strava_event_api_function/main.py
+++ src/handle_strava_event_api_function/main.py
@@ -1,56 +1,28 @@
 import json
-import os
 import logging
-import urllib3
+
+import strava_service
 
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
 
-HTTP = urllib3.PoolManager()
-
-URL = "https://www.strava.com"
-CLIENT_ID = 117583
-CLIENT_SECRET = "eabc93844d322f6a48a78f11adffbd9d94b7ebc9"
-REFRESH_TOKEN = "9eeb4aab45ca6a7ccff451c11bc6272875fe3eab"
-
-
-def _get_access_token():
-    data = {
-        "client_id": os.environ["STRAVA_CLIENT_ID"],
-        "client_secret": os.environ["STRAVA_CLIENT_SECRET"],
-        "grant_type": "refresh_token",
-        "refresh_token": os.environ["STRAVA_REFRESH_TOKEN"],
-    }
-    response = HTTP.request(
-        "POST", f"{os.environ['STRAVA_API_URL']}/oauth/token", data=data
-    )
-    print(response.json())
-    return response.json()["access_token"]
-
-
-def _get_my_activities(access_token: str):
-    response = requests.get(
-        f"{URL}/athlete/activities",
-        headers={"Authorization": f"Bearer {access_token}"},
-    )
-    return response.json()
-
-
-def main():
-    access_token = _get_access_token()
-
-    my_activities = _get_my_activities(access_token)
-
-    my_commute_rides = [
-        x for x in my_activities if x["sport_type"] == "Ride" and x["commute"]
-    ]
-
-    for x in my_commute_rides:
-        print(" - ".join([x["start_date"], x["name"]]))
-
 
 def lambda_handler(event, _) -> dict:
-    body = json.loads(event["body"])
-    LOGGER.info("Recieved Strava event: %s", body)
-    return {"statusCode": 200, "body": json.dumps({"status": "success"})}
+    try:
+        LOGGER.info("Recieved Strava event: %s", event["body"])
+
+        strava_event = json.loads(event["body"])
+
+        if strava_event["object_type"] != "activity":
+            return {"statusCode": 200, "body": json.dumps({"status": "success"})}
+
+        auth_token = strava_service.get_auth_token()
+        activity = strava_service.get_activity(strava_event["object_id"], auth_token)
+
+        strava_service.update_activity(activity, auth_token)
+
+        return {"statusCode": 200, "body": json.dumps({"status": "success"})}
+    except Exception:
+        LOGGER.exception("There was a failure.")
+        return {"statusCode": 400, "body": json.dumps({"status": "failed"})}
---
+++ src/handle_strava_event_api_function/strava_service.py
@@ -0,0 +1,63 @@
+import json
+import os
+from typing import Optional
+import urllib3
+
+
+HTTP = urllib3.PoolManager()
+
+
+def __send_http_request(
+    method: str, url: str, body: Optional[dict] = None, headers: Optional[dict] = None
+):
+    response = HTTP.request(method, url, body=json.dumps(body), headers=headers)
+
+    if response.status not in range(200, 300):
+        raise Exception(
+            f"Request failed: {response.status} - {response.data.decode('utf-8')}"
+        )
+
+    return json.loads(response.data.decode("utf-8"))
+
+
+def get_auth_token() -> str:
+    response = __send_http_request(
+        "POST",
+        f"{os.environ['STRAVA_API_URL']}/oauth/token",
+        body={
+            "client_id": os.environ["STRAVA_CLIENT_ID"],
+            "client_secret": os.environ["STRAVA_CLIENT_SECRET"],
+            "grant_type": "refresh_token",
+            "refresh_token": os.environ["STRAVA_REFRESH_TOKEN"],
+        },
+        headers={
+            "Content-Type": "application/json",
+        },
+    )
+    return response["access_token"]
+
+
+def get_activity(activity_id: int, auth_token: str) -> dict:
+    return __send_http_request(
+        "GET",
+        f"{os.environ['STRAVA_API_URL']}/activities/{activity_id}",
+        headers={"Authorization": f"Bearer {auth_token}"},
+    )
+
+
+def update_activity(activity: dict, auth_token: str) -> dict:
+    return __send_http_request(
+        "PUT",
+        f"{os.environ['STRAVA_API_URL']}/activities/{activity['id']}",
+        body={
+            "commute": activity["commute"],
+            "trainer": activity["trainer"],
+            "hide_from_home": activity["hide_from_home"],
+            "description": activity["description"],
+            "name": activity["name"],
+            "type": activity["type"],
+            "sport_type": activity["sport_type"],
+            "gear_id": activity["gear_id"],
+        },
+        headers={"Authorization": f"Bearer {auth_token}"},
+    )
