8352e8d81672c50815c311524cee18027d10db5e
GraysonCox <graysoncox98@gmail.com>
2024-11-07 18:20:56 -0600

    WIP

--- src/handle_strava_event_api_function/main.py
+++ src/handle_strava_event_api_function/main.py
@@ -1,4 +1,5 @@
 import json
+import os
 import logging
 import urllib3
 
@@ -8,28 +9,30 @@ LOGGER.setLevel(logging.INFO)
 
 HTTP = urllib3.PoolManager()
 
-URL = 'https://www.strava.com'
+URL = "https://www.strava.com"
 CLIENT_ID = 117583
-CLIENT_SECRET = 'eabc93844d322f6a48a78f11adffbd9d94b7ebc9'
-REFRESH_TOKEN = '9eeb4aab45ca6a7ccff451c11bc6272875fe3eab'
+CLIENT_SECRET = "eabc93844d322f6a48a78f11adffbd9d94b7ebc9"
+REFRESH_TOKEN = "9eeb4aab45ca6a7ccff451c11bc6272875fe3eab"
 
 
 def _get_access_token():
     data = {
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-        'grant_type': 'refresh_token',
-        'refresh_token': REFRESH_TOKEN,
+        "client_id": os.environ["STRAVA_CLIENT_ID"],
+        "client_secret": os.environ["STRAVA_CLIENT_SECRET"],
+        "grant_type": "refresh_token",
+        "refresh_token": os.environ["STRAVA_REFRESH_TOKEN"],
     }
-    response = requests.post(f'{URL}/api/v3/oauth/token', data=data)
+    response = HTTP.request(
+        "POST", f"{os.environ['STRAVA_API_URL']}/oauth/token", data=data
+    )
     print(response.json())
-    return response.json()['access_token']
+    return response.json()["access_token"]
 
 
 def _get_my_activities(access_token: str):
     response = requests.get(
-        f'{URL}/api/v3/athlete/activities',
-        headers={'Authorization': f'Bearer {access_token}'},
+        f"{URL}/athlete/activities",
+        headers={"Authorization": f"Bearer {access_token}"},
     )
     return response.json()
 
@@ -39,13 +42,15 @@ def main():
 
     my_activities = _get_my_activities(access_token)
 
-    my_commute_rides = [x for x in my_activities if x['sport_type'] == 'Ride' and x['commute']]
+    my_commute_rides = [
+        x for x in my_activities if x["sport_type"] == "Ride" and x["commute"]
+    ]
 
     for x in my_commute_rides:
-        print(' - '.join([x['start_date'], x['name']]))
+        print(" - ".join([x["start_date"], x["name"]]))
 
 
 def lambda_handler(event, _) -> dict:
     body = json.loads(event["body"])
     LOGGER.info("Recieved Strava event: %s", body)
-    return {"statusCode": 200, "body": json.dumps({"status": "success"})}import requests
+    return {"statusCode": 200, "body": json.dumps({"status": "success"})}
