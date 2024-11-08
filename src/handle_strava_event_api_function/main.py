ff6e36932812176560b03d36d9e85b3cef246659
GraysonCox <graysoncox98@gmail.com>
2024-11-07 23:26:58 -0600

    WIP

--- src/handle_strava_event_api_function/main.py
+++ src/handle_strava_event_api_function/main.py
@@ -20,9 +20,13 @@ def lambda_handler(event, _) -> dict:
         if not strava_event["commute"]:
             return {"statusCode": 200, "body": json.dumps({"status": "success"})}
 
+        LOGGER.info("The event is a commute activity and will now be hidden.")
         auth_token = strava_service.get_auth_token()
         activity = strava_service.get_activity(strava_event["object_id"], auth_token)
         activity["hide_from_home"] = True
+        activity["description"] = (
+            "This commute was automatically hidden by my shitty AWS project."
+        )
         strava_service.update_activity(activity, auth_token)
 
         return {"statusCode": 200, "body": json.dumps({"status": "success"})}
