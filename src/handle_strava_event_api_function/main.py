bf2cca9142506a6998b36b691c6561c77f45ca51
Grayson Cox <35706281+GraysonCox@users.noreply.github.com>
2024-11-09 13:58:01 -0600

    Hide commutes (#4)
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Please
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * More unit tests
    
    * More tests

--- src/handle_strava_event_api_function/main.py
+++ src/handle_strava_event_api_function/main.py
@@ -1,12 +1,49 @@
 import json
 import logging
 
+from . import strava_service
+
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
 
 
-def lambda_handler(event, _) -> dict:
-    body = json.loads(event["body"])
-    LOGGER.info("Recieved Strava event: %s", body)
+def __successful_response() -> dict:
     return {"statusCode": 200, "body": json.dumps({"status": "success"})}
+
+
+def __failed_response() -> dict:
+    return {"statusCode": 400, "body": json.dumps({"status": "failed"})}
+
+
+def lambda_handler(event: dict, _) -> dict:
+    try:
+        LOGGER.info("Recieved Strava event: %s", event["body"])
+
+        strava_event = json.loads(event["body"])
+
+        if any(
+            [
+                strava_event["aspect_type"] != "create",
+                strava_event["object_type"] != "activity",
+            ]
+        ):
+            return __successful_response()
+
+        auth_token = strava_service.get_auth_token()
+        activity = strava_service.get_activity(strava_event["object_id"], auth_token)
+
+        if not activity["commute"]:
+            return __successful_response()
+
+        LOGGER.info("The event is a commute activity and will now be hidden.")
+        activity["hide_from_home"] = True
+        activity["description"] = (
+            "This commute was automatically hidden by my shitty AWS project."
+        )
+        strava_service.update_activity(activity, auth_token)
+
+        return __successful_response()
+    except Exception:
+        LOGGER.exception("There was a failure.")
+        return __failed_response()
